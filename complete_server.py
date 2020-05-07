d1a51bf74c03ce815f55416fd06dcfec8d8d6cd1
Hugo Drakeskär <hugodrakeskar@gmail.com>
2020-05-07 18:00:13 +0200

    Added api and text routes

--- complete_server.py
+++ complete_server.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
@@ -24,11 +27,42 @@ def get_owner(reg_nr: str):
         owner = {'car': car, 'ort': ort, 'name': name, 'birth_year': year, 'url': owner_url}
         return owner
 
+def make_html(owner):
+    out = f"""
+    <!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+</head>
+
+<body>
+    <h1>CarOwner</h1>
+    <ul>
+        <li><h2>Bil: {owner['car']}</h2></li>
+        <li><h2>Ort: {owner['ort']}</h2></li>
+        <li><h2>Ägare: {owner['name']}</h2></li>
+        <li><h2>Födelseår: {owner['birth_year']}</h2></li>
+    </ul>
+    <a href={owner['url']}>Full länk</a>
+</body>
+</html>
+    """
+    return out
+
 
 class Server(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        if self.path[:4] == "/api":
+            self.send_header('Content-type', 'application/json')
+        else:
+            self.send_header('Content-type', 'text/html')
+        self.send_header('charset', 'utf-8')
         self.end_headers()
 
     def do_HEAD(self):
@@ -38,13 +72,19 @@ class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_headers()
         content = self.handle_http()
-        self.wfile.write(json.dumps(content).encode(encoding='utf_8'))
+        if self.path[:4] == "/api":
+            self.wfile.write(json.dumps(content).encode('utf-8'))
+        else:
+            self.wfile.write(make_html(content).encode('utf-8'))
+
 
     def handle_http(self):
         if self.path == "/":
             to_user = {"Hello World": "ok"}
         else:
-            to_user = {"info": get_owner(self.path.replace("/", ""))}
+            to_user = {"car": "Ford Fiesta 1.4 i", "ort": "Trollhättan", "name": "Krister Joakim Berg", "birth_year": "1968", "url": "http://www.merinfo.se/person/Trollh%C3%A4ttan/Krister-Joakim-Berg-1968/bptl7-2m8y8?utm_source=biluppgifter&utm_medium=owner&utm_term=person"}
+            #to_user = get_owner(self.path.replace("/", ""))
+
         return to_user
 
 
